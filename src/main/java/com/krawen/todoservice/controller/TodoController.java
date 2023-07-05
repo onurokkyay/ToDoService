0c2e0df8e0a7c3551a2156cba9ed7251623a43f5
onurokkyay <onurokyay1@gmail.com>
2023-07-05 14:48:01 +0300

    Retrieve Todo and Delete Todo added

--- src/main/java/com/krawen/todoservice/controller/TodoController.java
+++ src/main/java/com/krawen/todoservice/controller/TodoController.java
@@ -3,6 +3,8 @@ package com.krawen.todoservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,17 @@ public class TodoController {
 	@GetMapping("/users/{userName}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String userName) {
 		return todoService.findByUsername(userName);
+	}
+	
+	@GetMapping("/users/{userName}/todos/{id}")
+	public Todo retrieveTodo(@PathVariable String userName, @PathVariable int id) {
+		return todoService.findById(id);
+	}
+	
+	@DeleteMapping("/users/{userName}/todos/{id}")
+	public ResponseEntity<String> deleteTodo(@PathVariable String userName, @PathVariable int id) {
+		todoService.deleteById(id);
+		return ResponseEntity.ok("Deleted");
 	}
 
 }
