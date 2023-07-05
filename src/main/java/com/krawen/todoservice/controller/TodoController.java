ea0aaaebea818e17c3df252c349e128660a7a2d2
onurokkyay <onurokyay1@gmail.com>
2023-07-05 14:34:00 +0300

    Autowired fix

--- src/main/java/com/krawen/todoservice/controller/TodoController.java
+++ src/main/java/com/krawen/todoservice/controller/TodoController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.krawen.todoservice.service.ITodoService;
 import com.krawen.todoservice.todo.Todo;
-import com.krawen.todoservice.todo.TodoService;
 
 @RestController
 public class TodoController {
 	
 	@Autowired
-	private TodoService todoService;
+	private ITodoService todoService;
 	
 	@GetMapping("/users/{userName}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String userName) {
