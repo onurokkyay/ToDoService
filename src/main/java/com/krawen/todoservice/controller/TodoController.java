1adeb3a781569e8784a4850a649d662fde53f0d9
onurokkyay <onurokyay1@gmail.com>
2023-07-07 20:17:55 +0300

    Adding Update Todo and Create Todo

--- src/main/java/com/krawen/todoservice/controller/TodoController.java
+++ src/main/java/com/krawen/todoservice/controller/TodoController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.krawen.todoservice.service.ITodoService;
@@ -14,24 +16,36 @@ import com.krawen.todoservice.todo.Todo;
 
 @RestController
 public class TodoController {
-	
+
 	@Autowired
 	private ITodoService todoService;
-	
+
 	@GetMapping("/users/{userName}/todos")
 	public List<Todo> retrieveTodos(@PathVariable String userName) {
 		return todoService.findByUsername(userName);
 	}
-	
+
 	@GetMapping("/users/{userName}/todos/{id}")
 	public Todo retrieveTodo(@PathVariable String userName, @PathVariable int id) {
 		return todoService.findById(id);
 	}
-	
+
 	@DeleteMapping("/users/{userName}/todos/{id}")
 	public ResponseEntity<String> deleteTodo(@PathVariable String userName, @PathVariable int id) {
 		todoService.deleteById(id);
 		return ResponseEntity.ok("Deleted");
+	}
+
+	@PutMapping("/users/{userName}/todos/{id}")
+	public ResponseEntity<String> updateTodo(@PathVariable String userName, @PathVariable int id,
+			@RequestBody Todo todo) {
+		todoService.updateTodo(todo);
+		return ResponseEntity.ok("Updated");
+	}
+
+	@PutMapping("/users/{userName}/todos")
+	public Todo createTodo(@PathVariable String userName, @RequestBody Todo todo) {
+		return todoService.addTodo(todo.getUsername(), todo.getDescription(), todo.getTargetDate(), todo.isDone());
 	}
 
 }
