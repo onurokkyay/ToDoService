474acb2c847b9c9e229c8ff2a5903ddee59d22a0
onurokkyay <onurokyay1@gmail.com>
2023-07-13 17:53:55 +0300

    Todo entity and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id, description, done, target_date,username)
+values (1,'Learn Java', false, CURRENT_DATE(), 'onurokkyay');
+
+insert into todo (id, description, done, target_date,username)
+values (2,'Learn Spring Boot', false, CURRENT_DATE(), 'onurokkyay');
+
+insert into todo (id, description, done, target_date,username)
+values (3,'Learn Jpa', false, CURRENT_DATE(), 'onurokkyay');
